eaad42fc6a8ef009dc97b68b334aabe76461fd80
Shamshid <shmshd12@gmail.com>
2024-03-16 10:17:24 +0400

    set mysql and separate route

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 const fastify = require('fastify')({
   logger: true
 })
+fastify.register(require('@fastify/mysql'), {
+  connectionString: `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}`
+});
 
-fastify.get('/', function (request, reply) {
-  reply.send({ hello: 'world' })
-})
+fastify.register(require('./routes/root'));
+
+fastify.register(require('./routes/todos'));
 
 fastify.listen({ host: "0.0.0.0", port: 4000 }, function (err, address) {
   if (err) {
